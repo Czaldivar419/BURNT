5c23ab602d308ab99b8c6c268c7e45dcf95cb220
Christian Zaldivar <94251270+Czaldivar419@users.noreply.github.com>
2022-10-31 18:51:31 -0700

    Added MVP of map
    Created new method of plugging in user location. Map is now functional and ready for bug testing.

--- map.js
+++ map.js
@@ -22,77 +22,78 @@ function initMap() {
     console.log(lat);
     console.log(lng);
 
+    var coords = {lat, lng}
 
 
-  var where = ( "lat: " + lat + ", " + "lng: " + lng )
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: location,
-    zoom: 17,
-    mapId: "8d193001f940fde3",
-  });
-  // Create the places service.
-  const service = new google.maps.places.PlacesService(map);
-  let getNextPage;
-  const moreButton = document.getElementById("more");
 
-  moreButton.onclick = function () {
-    moreButton.disabled = true;
-    if (getNextPage) {
-      getNextPage();
+      // Create the map.
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coords,
+        zoom: 12,
+        mapId: "8d193001f940fde3",
+      });
+      // Create the places service.
+      const service = new google.maps.places.PlacesService(map);
+      let getNextPage;
+      const moreButton = document.getElementById("more");
+    
+      moreButton.onclick = function () {
+        moreButton.disabled = true;
+        if (getNextPage) {
+          getNextPage();
+        }
+      };
+    
+      // Perform a nearby search.
+      service.nearbySearch(
+        { location: coords, radius: 5000, type: "gym" },
+        (results, status, pagination) => {
+          if (status !== "OK" || !results) return;
+    
+          addPlaces(results, map);
+          moreButton.disabled = !pagination || !pagination.hasNextPage;
+          if (pagination && pagination.hasNextPage) {
+            getNextPage = () => {
+              // Note: nextPage will call the same handler function as the initial call
+              pagination.nextPage();
+            };
+          }
+        }
+      );
     }
-  };
-
-  // Perform a nearby search.
-  service.nearbySearch(
-    { location: where, radius: 50000, type: "gym" },
-    (results, status, pagination) => {
-      if (status !== "OK" || !results) return;
-
-      addPlaces(results, map);
-      moreButton.disabled = !pagination || !pagination.hasNextPage;
-      if (pagination && pagination.hasNextPage) {
-        getNextPage = () => {
-          // Note: nextPage will call the same handler function as the initial call
-          pagination.nextPage();
-        };
+    
+    function addPlaces(places, map) {
+      const placesList = document.getElementById("places");
+    
+      for (const place of places) {
+        if (place.geometry && place.geometry.location) {
+          const image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25),
+          };
+    
+          new google.maps.Marker({
+            map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location,
+          });
+    
+          const li = document.createElement("li");
+    
+          li.textContent = place.name;
+          placesList.appendChild(li);
+          li.addEventListener("click", () => {
+            map.setCenter(place.geometry.location);
+          });
+        }
       }
     }
-  );
-}
-
-function addPlaces(places, map) {
-  const placesList = document.getElementById("places");
-
-  for (const place of places) {
-    if (place.geometry && place.geometry.location) {
-      const image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-
-      new google.maps.Marker({
-        map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location,
-      });
-
-      const li = document.createElement("li");
-
-      li.textContent = place.name;
-      placesList.appendChild(li);
-      li.addEventListener("click", () => {
-        map.setCenter(place.geometry.location);
-      });
-    }
   }
-}
-}
-
-window.initMap = initMap;
+    window.initMap = initMap;
 
 
 
