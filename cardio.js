2c88c484a36a22ee651ebbbdc39cb6b527334292
Sublime-Sweetie <faith.Wallace.2@gmail.com>
2022-10-31 18:51:27 -0700

    monday merge

--- cardio.js
+++ cardio.js
@@ -145,7 +145,7 @@ function createWorkout() {
          wedSplit.append(...cooldown);
          friSplit.append(...cooldown);
       }else if(stretchInput === "") {
-         alert("Cooldown: Yes or no?")
+         // alert("Cooldown: Yes or no?")
       }else{
          monSplit.append("No cooldown")
          wedSplit.append("No cooldown")
